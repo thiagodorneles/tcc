a204a88096d5007be84ec9f662bda93cc7d5ef95
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-11-26 15:43:22 -0200

    Código da tela de detalhes

--- ios/TCC/TCC/Views/DetailViewController.h
+++ ios/TCC/TCC/Views/DetailViewController.h
@@ -7,7 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Publish.h"
+//#import <UIKit/UIActivityViewController.h>
 
-@interface DetailViewController : UITableViewController
+@interface DetailViewController : UITableViewController <UITableViewDataSource>
+
+@property Publish *publish;
+@property (weak, nonatomic) IBOutlet UILabel *labelTitle;
+@property (weak, nonatomic) IBOutlet UILabel *labelUser;
+@property (weak, nonatomic) IBOutlet UILabel *labelDate;
+@property (weak, nonatomic) IBOutlet UILabel *labelTags;
+@property (weak, nonatomic) IBOutlet UITextView *textDescription;
+@property (weak, nonatomic) IBOutlet UIImageView *imagePicture;
+//@property (weak, nonatomic) IBOutlet UIBarButtonItem *buttonShare;
+//@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+//- (IBAction)buttonSharedTouched:(id)sender;
 
 @end
