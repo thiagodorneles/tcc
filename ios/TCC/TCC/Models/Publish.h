5431a8b403a69e4f934c94bb1f91a8d39acbe2f8
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-11-26 15:42:25 -0200

    Modelo para serializar objeto da API

--- ios/TCC/TCC/Models/Publish.h
+++ ios/TCC/TCC/Models/Publish.h
@@ -7,7 +7,26 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface Publish : NSObject
+@interface Publish : NSObject <UIActivityItemSource>
+
+@property NSString *title;
+@property NSString *description;
+@property NSString *created_at;
+@property NSDate *date;
+@property NSString *location;
+@property NSString *city;
+@property NSInteger quant_views;
+@property NSInteger quant_blocks;
+@property NSString *user;
+@property NSMutableArray *tags;
+
+// Location
+@property float latitude;
+@property float longitude;
+@property CLLocationCoordinate2D coordinate;
+
 
 @end
--- ios/TCC/TCC/Models/Publish.m
+++ ios/TCC/TCC/Models/Publish.m
@@ -10,4 +10,19 @@
 
 @implementation Publish
 
+@synthesize title, description, created_at, date, location, city, user, tags, quant_views, quant_blocks;
+
+- (id)activityViewControllerPlaceholderItem:(UIActivityViewController *)activityViewController;
+{
+    return self.title;
+}
+// called to determine data type. only the class of the return type is consulted. it should match what -itemForActivityType: returns later
+- (id)activityViewController:(UIActivityViewController *)activityViewController itemForActivityType:(NSString *)activityType
+{
+    return self.title;
+}
+// called to fetch data after an activity is selected. you can return nil.
+
+
+
 @end
