f2870964219a64c09e3da7275ad45cd446e008eb
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-11-26 20:55:44 -0200

    Inclusão do model User

---
+++ ios/TCC/TCC/Models/User.h
@@ -0,0 +1,17 @@
+//
+//  User.h
+//  TCC
+//
+//  Created by Thiago Dorneles on 11/26/13.
+//  Copyright (c) 2013 Thiago Dorneles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property NSInteger pk;
+@property NSString *name;
+@property NSString *email;
+
+@end
---
+++ ios/TCC/TCC/Models/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  TCC
+//
+//  Created by Thiago Dorneles on 11/26/13.
+//  Copyright (c) 2013 Thiago Dorneles. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
