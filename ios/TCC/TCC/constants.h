e1e1098c8e475bb73c41fbbc09ae8d4a1579c019
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-12-06 01:08:21 -0200

    usando servidor local

--- ios/TCC/TCC/constants.h
+++ ios/TCC/TCC/constants.h
@@ -9,8 +9,8 @@
 #ifndef TCC_constants_h
 #define TCC_constants_h
 
-//#define URL_SERVER @"http://127.0.0.1:8000/api/"
-#define URL_SERVER @"http://plataformaugc.herokuapp.com/api/"
+#define URL_SERVER @"http://127.0.0.1:8000/api/"
+//#define URL_SERVER @"http://plataformaugc.herokuapp.com/api/"
 #define URL_DEFAULT @"/publishs/"
 #define URL_SEARCH @"/search/"
 
