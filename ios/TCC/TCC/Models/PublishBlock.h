7b0ab2be2eb2bd79f2e5a61b6f1db704c3ca2a32
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-12-08 21:46:17 -0200

    Classe para gerenciar POST de bloqueio da noticia

---
+++ ios/TCC/TCC/Models/PublishBlock.h
@@ -0,0 +1,18 @@
+//
+//  PublishBlock.h
+//  TCC
+//
+//  Created by Thiago Dorneles on 12/8/13.
+//  Copyright (c) 2013 Thiago Dorneles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface PublishBlock : NSObject
+
+@property NSInteger pk;
+@property NSInteger quant_blocks;
+
+@end
---
+++ ios/TCC/TCC/Models/PublishBlock.m
@@ -0,0 +1,15 @@
+//
+//  PublishBlock.m
+//  TCC
+//
+//  Created by Thiago Dorneles on 12/8/13.
+//  Copyright (c) 2013 Thiago Dorneles. All rights reserved.
+//
+
+#import "PublishBlock.h"
+
+@implementation PublishBlock
+
+@synthesize pk, quant_blocks;
+
+@end
