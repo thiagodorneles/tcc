9e1c7143f86105ac8808bb41268c77e8d24d7c15
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-12-09 23:42:56 -0200

    Categorias para utilização em imagens

---
+++ ios/TCC/Categories/UIImage+fixOrientation.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+fixOrientation.h
+//  TCC
+//
+//  Created by Thiago Dorneles on 12/9/13.
+//  Copyright (c) 2013 Thiago Dorneles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIImage (fixOrientation)
+
+- (UIImage *)fixOrientation;
+
+@end
